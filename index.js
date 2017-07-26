5e6880f2523d53da7e61657d6e0a648258e91d70
Carlis Liu <carlisliu@live.com>
2017-07-26 18:03:45 +0800

    add impls

---
+++ index.js
@@ -0,0 +1,59 @@
+function AysncContext() {
+    this.active = null;
+    this._set = [];
+}
+
+AysncContext.prototype = {
+    createContext: function() {
+        return Object.create(this.active);
+    },
+    get: function(key) {
+        if (this.active) {
+            return this.active[key];
+        }
+        throw new Error('no context is available!');
+    },
+    set: function(key, value) {
+        if (this.active) {
+            return (this.active[key] = value);
+        }
+    },
+    enter: function(context) {
+        if (!context) {
+            throw new Error('context required!');
+        }
+        this._set.push(this.active);
+        this.active = context;
+    },
+    exit: function(context) {
+        if (!context) {
+            throw new Error('context required!');
+        }
+        if (context === this.active) {
+            this.active = this._set.pop();
+            return;
+        }
+
+        var index = this._set.lastIndexOf(context);
+        if (index < 0) {
+            throw new Error('context not currently entered!');
+        }
+        this._set.splice(index, 1);
+    },
+    bind: function(fn, context) {
+        if (!context) {
+            context = this.active ? this.active : this.createContext();
+        }
+        var self = this;
+        return function() {
+            self.enter(context);
+            try {
+                return fn.apply(this, arguments);
+            } catch (exception) {
+                throw exception;
+            } finally {
+                self.exit(context);
+            }
+        }
+    }
+};
